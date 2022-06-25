24adc0ea2c13a804a023f951692b67e688944d24
goneyacine <goneyacine@gmail.com>
2022-06-25 11:20:50 +0100

    declaring some functions

--- Pixel++/Pixelpp.h
+++ Pixel++/Pixelpp.h
@@ -29,11 +29,23 @@ namespace Pixelpp
 	*/
 	uint16_t* GetRGB16(uint64_t p_rgba);
 
-
 	/*
 	* extracts the red, green, blue & alpha from 64 bit rgba color and returns the result in an array
 	*/
 	uint16_t* GetRGBA16(uint16_t p_rgba);
+
+
+	void SetRed16(uint64_t* p_rgba,uint16_t p_red);
+
+	void SetGreen16(uint64_t* p_rgba,uint16_t p_green);
+
+	void SetBlue16(uint64_t* p_rgba,uint16_t p_blue);
+
+	void SetAlpha16(uint64_t* p_rgba,uint16_t p_alpha);
+
+	void SetRGB16(uint64_t* p_rgba, uint16_t p_red, uint16_t p_green, uint16_t p_blue);
+
+	void SetRGBA16(uint64_t* p_rgba, uint16_t p_red, uint16_t p_green, uint16_t p_blue,uint16_t p_alpha);
 
 
 	/*-------------- 32 BIT COLOR (8 BIT CHANNELS) -------------- */
@@ -66,6 +78,20 @@ namespace Pixelpp
 	*/
 	uint8_t* GetRGBA8(uint32_t p_rgba);
 
+
+
+	void SetRed8(uint32_t* p_rgba, uint8_t p_red);
+
+	void SetGreen8(uint32_t* p_rgba, uint8_t p_green);
+
+	void SetBlue8(uint32_t* p_rgba, uint8_t p_blue);
+
+	void SetAlpha8(uint32_t* p_rgba, uint8_t p_alpha);
+
+	void SetRGB8(uint32_t* p_rgba, uint8_t p_red, uint8_t p_green, uint8_t p_blue);
+
+	void SetRGBA8(uint32_t* p_rgba, uint8_t p_red, uint8_t p_green, uint8_t p_blue, uint8_t p_alpha);
+
 	/*-------------- 16 BIT COLOR (4 BIT CHANNELS) -------------- */
     
 	/*
@@ -95,4 +121,7 @@ namespace Pixelpp
 	* extracts the red, green, blue & alpha from 16 bit rgba color and returns the result in an array
 	*/
 	uint8_t* GetRGBA8(uint16_t p_rgba);
+
+
+
 }
