138fa1743b8f95df7050fb971e31803b9ff87bc5
goneyacine <goneyacine@gmail.com>
2022-06-27 16:29:44 +0100

    writting some tests

--- Tests/Main.cpp
+++ Tests/Main.cpp
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <cstdlib>
 
-#define CASE_NUM 1000
+#define CASE_NUM 10000
 
 
 /* TESTING THE GET RED FUNCTIONS */
@@ -38,8 +38,102 @@ TEST(GetRed4Test, CorrectRedValueTest)
     }
 }
 
-
 /* TESTING THE GET GREEN FUNCTIONS */
+
+TEST(GetGreen16Test, CorrectGreenValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint16_t randomGreenValue = (uint16_t)(rand() / RAND_MAX) * 0xFFFF;
+        uint64_t rgba = ((uint64_t)randomGreenValue) << 32;
+        ASSERT_EQ(randomGreenValue, Pixelpp::GetGreen16(rgba));
+    }
+}
+
+TEST(GetGreen8Test, CorrectGreenValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint8_t randomGreenValue = (uint8_t)(rand() / RAND_MAX) * 0xFF;
+        uint32_t rgba = ((uint32_t)randomGreenValue) << 16;
+        ASSERT_EQ(randomGreenValue, Pixelpp::GetGreen8(rgba));
+    }
+}
+
+TEST(GetGreen4Test, CorrectGreenValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint8_t randomGreenValue = (uint8_t)(rand() / RAND_MAX) * 0x0F;
+        uint16_t rgba = ((uint16_t)randomGreenValue) << 8;
+        ASSERT_EQ(randomGreenValue, Pixelpp::GetGreen4(rgba));
+    }
+}
+
+/* TESTING THE GET BLUE FUNCTIONS */
+
+TEST(GetBlue16Test, CorrectBlueValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint16_t randomBlueValue = (uint16_t)(rand() / RAND_MAX) * 0xFFFF;
+        uint64_t rgba = ((uint64_t)randomBlueValue) << 16;
+        ASSERT_EQ(randomBlueValue, Pixelpp::GetBlue16(rgba));
+    }
+}
+
+TEST(GetBlue8Test, CorrectBlueValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint8_t randomBlueValue = (uint8_t)(rand() / RAND_MAX) * 0xFF;
+        uint32_t rgba = ((uint32_t)randomBlueValue) << 8;
+        ASSERT_EQ(randomBlueValue, Pixelpp::GetBlue8(rgba));
+    }
+}
+
+TEST(GetBlue4Test, CorrectBlueValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint8_t randomBlueValue = (uint8_t)(rand() / RAND_MAX) * 0x0F;
+        uint16_t rgba = ((uint16_t)randomBlueValue) << 4;
+        ASSERT_EQ(randomBlueValue, Pixelpp::GetBlue4(rgba));
+    }
+}
+
+
+/* TESTING THE GET ALPHA FUNCTIONS */
+
+TEST(GetAlpha16Test, CorrectAlphaValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint16_t randomAlphaValue = (uint16_t)(rand() / RAND_MAX) * 0xFFFF;
+        uint64_t rgba = ((uint64_t)randomAlphaValue);
+        ASSERT_EQ(randomAlphaValue, Pixelpp::GetAlpha16(rgba));
+    }
+}
+
+TEST(GetAlpha8Test, CorrectAlphaValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint8_t randomAlphaValue = (uint8_t)(rand() / RAND_MAX) * 0xFF;
+        uint32_t rgba = ((uint32_t)randomAlphaValue);
+        ASSERT_EQ(randomAlphaValue, Pixelpp::GetAlpha8(rgba));
+    }
+}
+
+TEST(GetAlpha4Test, CorrectAlphaValueTest)
+{
+    for (int i = 0; i < CASE_NUM; i++)
+    {
+        uint8_t randomAlphaValue = (uint8_t)(rand() / RAND_MAX) * 0x0F;
+        uint16_t rgba = ((uint16_t)randomAlphaValue);
+        ASSERT_EQ(randomAlphaValue, Pixelpp::GetAlpha4(rgba));
+    }
+}
 
 
 
